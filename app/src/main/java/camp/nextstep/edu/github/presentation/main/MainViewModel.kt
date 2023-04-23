dbc0c9efdd1af4f5f128a31ed0dca09f228f3cd4
Hyunkuk <boris0815@naver.com>
2023-04-24 05:53:37 +0900

    [feat] : @Inject 어노테이션 추가

---
+++ app/src/main/java/camp/nextstep/edu/github/presentation/main/MainViewModel.kt
@@ -0,0 +1,24 @@
+package camp.nextstep.edu.github.presentation.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import camp.nextstep.edu.github.domain.GithubSearchRepository
+import camp.nextstep.edu.github.domain.model.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class MainViewModel @Inject constructor(
+    private val gitHubSearchRepository: GithubSearchRepository,
+) : ViewModel() {
+    private val _repositories = MutableLiveData<List<Repository>>()
+    val repositories: LiveData<List<Repository>>
+        get() = _repositories
+
+    fun searchRepositories() = viewModelScope.launch(Dispatchers.IO) {
+        val repositories = gitHubSearchRepository.searchGitHubs()
+        _repositories.postValue(repositories)
+    }
+}
