a0c356762f78962cd559bf91c766cac6841f53d3
Hyunkuk <boris0815@naver.com>
2023-04-24 05:53:08 +0900

    [feat]: app , presentation component 생성

---
+++ app/src/main/java/camp/nextstep/edu/github/GitHubApplication.kt
@@ -0,0 +1,20 @@
+package camp.nextstep.edu.github
+
+import android.app.Application
+import camp.nextstep.edu.github.di.AppComponent
+import camp.nextstep.edu.github.di.DaggerAppComponent
+import camp.nextstep.edu.github.presentation.PresentationComponent
+
+/**
+ * @Created by 김현국 2023/04/24
+ * @Time 1:39 AM
+ */
+class GitHubApplication : Application() {
+    val appComponent: AppComponent by lazy {
+        DaggerAppComponent.factory().create(applicationContext)
+    }
+
+    val presentationComponent: PresentationComponent by lazy {
+        appComponent.presentationComponent().create()
+    }
+}
