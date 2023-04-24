f412a33ff6d85564542ac6c2846bc4a0972717d4
SEO-J17 <qw4643987@naver.com>
2023-04-25 01:09:12 +0900

    :sparkles: ViewModelFactory 생성

---
+++ app/src/main/java/camp/nextstep/edu/github/ui/ViewModelFactory.kt
@@ -0,0 +1,27 @@
+package camp.nextstep.edu.github.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.MapKey
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+import kotlin.reflect.KClass
+
+@Singleton
+class ViewModelFactory @Inject constructor(
+    private val viewModels: MutableMap<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>,
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return viewModels[modelClass]?.get() as T
+    }
+
+    @Target(
+        AnnotationTarget.FUNCTION,
+        AnnotationTarget.PROPERTY_GETTER,
+        AnnotationTarget.PROPERTY_SETTER,
+    )
+    @Retention(AnnotationRetention.RUNTIME)
+    @MapKey
+    internal annotation class ViewModelKey(val value: KClass<out ViewModel>)
+}
