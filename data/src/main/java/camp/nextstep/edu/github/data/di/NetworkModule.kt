e09282c17257413980e7d93833936cae5300f978
SEO-J17 <qw4643987@naver.com>
2023-04-25 01:08:32 +0900

    :sparkles: 네트워크 모듈 추가

---
+++ data/src/main/java/camp/nextstep/edu/github/data/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package camp.nextstep.edu.github.data.di
+
+import camp.nextstep.edu.github.data.Constants
+import camp.nextstep.edu.github.data.api.GitHubSearchApi
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+    ): Retrofit {
+        return Retrofit
+            .Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
+            .baseUrl(Constants.BASE_URL)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGitHubSearchApi(
+        retrofit: Retrofit,
+    ): GitHubSearchApi {
+        return retrofit.create(GitHubSearchApi::class.java)
+    }
+}
