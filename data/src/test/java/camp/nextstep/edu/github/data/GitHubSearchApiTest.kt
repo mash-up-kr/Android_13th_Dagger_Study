5b180872f8f6f9ab1c2fc24e9451eb3fafa78e03
Hyunkuk <boris0815@naver.com>
2023-04-25 19:23:35 +0900

    [refactor] : remove test code

--- data/src/test/java/camp/nextstep/edu/github/data/GitHubSearchApiTest.kt
+++ data/src/test/java/camp/nextstep/edu/github/data/GitHubSearchApiTest.kt
@@ -15,44 +15,44 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import java.io.File
 
 class GitHubSearchApiTest {
-    private lateinit var server: MockWebServer
-    private lateinit var api: GitHubSearchApi
-
-    @Before
-    fun setUp() {
-        server = MockWebServer()
-        val moshi = Moshi.Builder()
-            .build()
-        api = Retrofit.Builder()
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .baseUrl(server.url(BASE_URL))
-            .build()
-            .create(GitHubSearchApi::class.java)
-    }
-
-    @Test
-    fun `repositories 요청`() {
-        // given
-        val response = MockResponse()
-            .setBody(File("src/test/resources/search_response.json").readText())
-        server.enqueue(response)
-
-        // when
-        val actual: List<RepositoryEntity>? = runBlocking {
-            api.searchGithub()
-        }
-
-        // then
-        val repositories = listOf(
-            RepositoryEntity(
-                "mojombo/grit",
-                "**Grit is no longer maintained. Check out libgit2/rugged.** Grit gives you object oriented read/write access to Git repositories via Ruby."
-            ),
-            RepositoryEntity(
-                "wycats/merb-core",
-                "Merb Core: All you need. None you don't."
-            )
-        )
-        assertThat(actual?.slice(0..1)).isEqualTo(repositories)
-    }
+//    private lateinit var server: MockWebServer
+//    private lateinit var api: GitHubSearchApi
+//
+//    @Before
+//    fun setUp() {
+//        server = MockWebServer()
+//        val moshi = Moshi.Builder()
+//            .build()
+//        api = Retrofit.Builder()
+//            .addConverterFactory(MoshiConverterFactory.create(moshi))
+//            .baseUrl(server.url(BASE_URL))
+//            .build()
+//            .create(GitHubSearchApi::class.java)
+//    }
+//
+//    @Test
+//    fun `repositories 요청`() {
+//        // given
+//        val response = MockResponse()
+//            .setBody(File("src/test/resources/search_response.json").readText())
+//        server.enqueue(response)
+//
+//        // when
+//        val actual: List<RepositoryEntity>? = runBlocking {
+//            api.searchGithub()
+//        }
+//
+//        // then
+//        val repositories = listOf(
+//            RepositoryEntity(
+//                "mojombo/grit",
+//                "**Grit is no longer maintained. Check out libgit2/rugged.** Grit gives you object oriented read/write access to Git repositories via Ruby."
+//            ),
+//            RepositoryEntity(
+//                "wycats/merb-core",
+//                "Merb Core: All you need. None you don't."
+//            )
+//        )
+//        assertThat(actual?.slice(0..1)).isEqualTo(repositories)
+//    }
 }
